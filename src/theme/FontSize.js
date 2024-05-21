8331e40ca0b9a85155dc1caaa5ff58b619cbd253
breduarm <bryan.e.armijos@gmail.com>
2024-05-21 15:00:00 -0500

    Update theming according with Material 3

--- src/theme/FontSize.js
+++ src/theme/FontSize.js
@@ -1,7 +1,10 @@
 const FontSize = {
   small: 8,
-  medium: 12,
+  medium: 16,
   large: 24,
+  xLarge: 32,
+  xxLarge: 40,
+  xxxLarge: 48,
 };
 
 export default FontSize;
--- src/theme/Spacing.js
+++ src/theme/Spacing.js
@@ -1,10 +1,14 @@
+const basicUnit = 8 // in pixels
+
 const Spacing = {
-  unit: 8, // Basic unit in pixeles
-  space8: 1 * Spacing.unit,
-  space16: 2 * Spacing.unit,
-  space24: 3 * Spacing.unit,
-  space32: 4 * Spacing.unit,
-  space40: 5 * Spacing.unit,
+  space8: 1 * basicUnit,
+  space16: 2 * basicUnit,
+  space24: 3 * basicUnit,
+  space32: 4 * basicUnit,
+  space40: 5 * basicUnit,
+  space48: 6 * basicUnit,
 };
+
+const a = Spacing.space16
 
 export default Spacing;
